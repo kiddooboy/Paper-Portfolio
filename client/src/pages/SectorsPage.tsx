11685ff88575f0a84193d72d7ea289b9e223cb05
Yogesh Nithyanandam <yogesh.nithyanandam@gmail.com>
2026-05-02 16:54:41 +0530

    fix: remove unused formatCurrency import in SectorsPage

--- client/src/pages/SectorsPage.tsx
+++ client/src/pages/SectorsPage.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 import axios from 'axios';
-import { cn, formatCurrency } from '../lib/utils';
+import { cn } from '../lib/utils';
 
 interface SectorQuote {
   name: string;
